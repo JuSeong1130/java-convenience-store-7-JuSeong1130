13d640dc0d3e98dc7a40315bd3886d39e63ba421
JuSeong1130 <jsmoon1130@naver.com>
2024-11-10 20:41:35 +0900

    feat(Input) : 멤버십 할인 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -15,8 +15,9 @@ public class InputView {
 
 
     private static final String ASK_PRODUCT_AND_QUANTITY = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String ASK_MEMBERSHIP_DISCOUNT_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
 
-    public RequestOrder readItem() {
+    public RequestOrder askItem() {
         System.out.println(ASK_PRODUCT_AND_QUANTITY);
         String input = readLine();
         String fullPattern = "\\[([가-힣]+)-([0-9]+)\\](,\\[([가-힣]+)-([0-9]+)\\])*";
@@ -36,6 +37,11 @@ public class InputView {
             orderProducts.add(new RequestOrderProduct(name, quantity));
         }
         return new RequestOrder(orderProducts);
+    }
+
+    public String askMemberShipDiscount() {
+        System.out.println(ASK_MEMBERSHIP_DISCOUNT_MESSAGE);
+        return readLine();
     }
 
 
