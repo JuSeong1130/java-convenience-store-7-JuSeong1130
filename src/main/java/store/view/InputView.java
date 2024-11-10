f40084e6ce00e99b759f389d1681b41e08b45188
JuSeong1130 <jsmoon1130@naver.com>
2024-11-10 20:46:05 +0900

    feat(Input) : 제품 제외 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -18,6 +18,7 @@ public class InputView {
     private static final String ASK_MEMBERSHIP_DISCOUNT_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
     private static final String ASK_ADDITIONAL_PURCHASE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
     private static final String ASK_FREE_ITEM_ADDITION_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+    private static final String ASK_PRODUCT_EXCLUSION_CONFIRMATION_MESSAGE = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
 
     public RequestOrder askItem() {
         System.out.println(ASK_PRODUCT_AND_QUANTITY);
@@ -56,5 +57,9 @@ public class InputView {
         return readLine();
     }
 
+    public String askProductExclusion() {
+        System.out.println(ASK_PRODUCT_EXCLUSION_CONFIRMATION_MESSAGE);
+        return readLine();
+    }
 
 }
