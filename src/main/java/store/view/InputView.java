1f04e3e726c2dd6dd3fc922155df061f52564e70
JuSeong1130 <jsmoon1130@naver.com>
2024-11-10 20:42:35 +0900

    feat(Input) : 구매 계속 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -16,6 +16,7 @@ public class InputView {
 
     private static final String ASK_PRODUCT_AND_QUANTITY = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String ASK_MEMBERSHIP_DISCOUNT_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String ASK_ADDITIONAL_PURCHASE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public RequestOrder askItem() {
         System.out.println(ASK_PRODUCT_AND_QUANTITY);
@@ -41,6 +42,11 @@ public class InputView {
 
     public String askMemberShipDiscount() {
         System.out.println(ASK_MEMBERSHIP_DISCOUNT_MESSAGE);
+        return readLine();
+    }
+
+    public String askAdditionalPurchase() {
+        System.out.println(ASK_ADDITIONAL_PURCHASE_MESSAGE);
         return readLine();
     }
 
