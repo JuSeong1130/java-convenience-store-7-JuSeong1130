5870ab99dee0aed3190c2bc248c560c9e18a2a61
JuSeong1130 <jsmoon1130@naver.com>
2024-11-10 20:43:30 +0900

    feat(Input) : 증정상품 제공 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -17,6 +17,7 @@ public class InputView {
     private static final String ASK_PRODUCT_AND_QUANTITY = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String ASK_MEMBERSHIP_DISCOUNT_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
     private static final String ASK_ADDITIONAL_PURCHASE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
+    private static final String ASK_FREE_ITEM_ADDITION_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     public RequestOrder askItem() {
         System.out.println(ASK_PRODUCT_AND_QUANTITY);
@@ -47,6 +48,11 @@ public class InputView {
 
     public String askAdditionalPurchase() {
         System.out.println(ASK_ADDITIONAL_PURCHASE_MESSAGE);
+        return readLine();
+    }
+
+    public String askFreeItemAddition() {
+        System.out.println(ASK_FREE_ITEM_ADDITION_MESSAGE);
         return readLine();
     }
 
