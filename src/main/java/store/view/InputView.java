0f494f35a0ce9ecd20f323438d1ca5a739087871
JuSeong1130 <jsmoon1130@naver.com>
2024-11-11 19:55:52 +0900

    refactor(InputView) : askItem 함수 분리 및 검토 요청 함수 리팩토링

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,5 +1,6 @@
 package store.view;
 
+import store.controller.dto.StateContextDTO;
 import store.view.dto.RequestOrder;
 import store.view.dto.RequestOrderProduct;
 
@@ -19,17 +20,27 @@ public class InputView {
     private static final String ASK_ADDITIONAL_PURCHASE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
     private static final String ASK_FREE_ITEM_ADDITION_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     private static final String ASK_PRODUCT_EXCLUSION_CONFIRMATION_MESSAGE = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+    public static final String WRONG_FORMAT_EXCEPTION = "잘못된 형식을 입력하셨습니다. 다시 입력해주세요.";
 
     public RequestOrder askItem() {
         System.out.println(ASK_PRODUCT_AND_QUANTITY);
         String input = readLine();
-        String fullPattern = "\\[([가-힣]+)-([0-9]+)\\](,\\[([가-힣]+)-([0-9]+)\\])*";
         String itemPattern = "\\[([가-힣]+)-([0-9]+)\\]";
 
-        if (!input.matches(fullPattern)) {
-            throw new IllegalArgumentException("잘못된 형식을 입력하셨습니다");
-        }
+        validateOrder(input);
 
+        List<RequestOrderProduct> orderProducts = getRequestOrderProducts(itemPattern, input);
+        return new RequestOrder(orderProducts);
+    }
+
+    private void validateOrder(String input) {
+        String fullPattern = "\\[([가-힣]+)-([0-9]+)\\](,\\[([가-힣]+)-([0-9]+)\\])*";
+        if (!input.matches(fullPattern)) {
+            throw new IllegalArgumentException(WRONG_FORMAT_EXCEPTION);
+        }
+    }
+
+    private static List<RequestOrderProduct> getRequestOrderProducts(String itemPattern, String input) {
         List<RequestOrderProduct> orderProducts = new ArrayList<>();
         Pattern p = Pattern.compile(itemPattern);
         Matcher m = p.matcher(input);
@@ -39,7 +50,7 @@ public class InputView {
             int quantity = Integer.parseInt(m.group(2));  // 수량
             orderProducts.add(new RequestOrderProduct(name, quantity));
         }
-        return new RequestOrder(orderProducts);
+        return orderProducts;
     }
 
     public String askMemberShipDiscount() {
@@ -52,13 +63,24 @@ public class InputView {
         return readLine();
     }
 
-    public String askFreeItemAddition() {
-        System.out.println(ASK_FREE_ITEM_ADDITION_MESSAGE);
+    public String askReviewPending(StateContextDTO stateContext) {
+        if(stateContext.status().equals("GIFT")){
+            return askFreeItemAddition(stateContext);
+        }
+
+        if(stateContext.status().equals("EXCLUSION")) {
+            return askProductExclusion(stateContext);
+        }
+        return "";
+    }
+
+    private String askFreeItemAddition(StateContextDTO stateContext) {
+        System.out.println(ASK_FREE_ITEM_ADDITION_MESSAGE.formatted(stateContext.name(), stateContext.quantity()));
         return readLine();
     }
 
-    public String askProductExclusion() {
-        System.out.println(ASK_PRODUCT_EXCLUSION_CONFIRMATION_MESSAGE);
+    private String askProductExclusion(StateContextDTO stateContext) {
+        System.out.println(ASK_PRODUCT_EXCLUSION_CONFIRMATION_MESSAGE.formatted(stateContext.name(), stateContext.quantity()));
         return readLine();
     }
 
