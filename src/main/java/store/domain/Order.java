720995e6efccbb7ab3a2a5a3b515618638945225
JuSeong1130 <jsmoon1130@naver.com>
2024-11-11 20:03:59 +0900

    feat(Order) : solvePending 기능 구현

--- src/main/java/store/domain/Order.java
+++ src/main/java/store/domain/Order.java
@@ -14,4 +14,11 @@ public class Order {
         this.hasMembershipDiscount = false;
     }
 
+    public void solvePending(String name,Command command) {
+        OrderProduct orderProduct = orderProducts.stream()
+                .filter(product -> product.getName().equals(name))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("잘못된 상품명을 입력하셨습니다"));
+        orderProduct.solvePending(command);
+    }
+
 }
