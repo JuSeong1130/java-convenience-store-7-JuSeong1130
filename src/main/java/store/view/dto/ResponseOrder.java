f7ebe58cf65df1b981cf9df55e4ecaee7016e1d8
JuSeong1130 <jsmoon1130@naver.com>
2024-11-11 20:17:07 +0900

    feat(ResponseOrder) : 변환하는 from 기능 구현

--- src/main/java/store/view/dto/ResponseOrder.java
+++ src/main/java/store/view/dto/ResponseOrder.java
@@ -1,6 +1,23 @@
 package store.view.dto;
 
+import store.domain.OrderGift;
+import store.domain.OrderPayment;
+import store.domain.OrderProduct;
+import store.domain.OrderReceipt;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public record ResponseOrder(List<ResponseOrderProduct> orderProducts, List<ResponseGift> gifts, ResponseReceipt receipt) {
+    public static ResponseOrder from(OrderReceipt orderReceipt) {
+        List<ResponseOrderProduct> responseOrderProducts = new ArrayList<>();
+        List<OrderProduct> orderProducts = orderReceipt.orderProducts();
+        orderProducts.forEach(orderProduct -> responseOrderProducts.add(ResponseOrderProduct.from(orderProduct)));
+        List<ResponseGift> responseGifts = new ArrayList<>();
+        List<OrderGift> orderGifts = orderReceipt.orderGifts();
+        orderGifts.forEach(orderGift -> responseGifts.add(ResponseGift.from(orderGift)));
+        OrderPayment orderPayment = orderReceipt.orderPayment();
+        ResponseReceipt responseReceipt = new ResponseReceipt(orderPayment.totalCount(), orderPayment.totalPrice(), orderPayment.promotionDiscountPrice(), orderPayment.membershipDiscountPrice(), orderPayment.payPrice());
+        return new ResponseOrder(responseOrderProducts, responseGifts, responseReceipt);
+    }
 }
