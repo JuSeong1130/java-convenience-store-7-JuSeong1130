f40a6390bf421bd33276523a4f04180ff37e74c6
JuSeong1130 <jsmoon1130@naver.com>
2024-11-11 19:01:12 +0900

    feat(AppConfig) : MVC 객체 생성 기능 구현

---
+++ src/main/java/store/config/AppConfig.java
@@ -0,0 +1,49 @@
+package store.config;
+
+import store.controller.OrderController;
+import store.domain.Product;
+import store.repository.OrderRepository;
+import store.repository.ProductRepository;
+import store.service.OrderService;
+import store.service.ProductService;
+import store.util.ProductLoader;
+import store.view.InputView;
+import store.view.OutputView;
+
+import java.util.List;
+
+public class AppConfig {
+
+    private static ProductService getProductService() {
+        return new ProductService(getProductRepository());
+    }
+
+    private static ProductRepository getProductRepository() {
+        return new ProductRepository(getProduct());
+    }
+
+    private static List<Product> getProduct() {
+        return ProductLoader.loadProducts();
+    }
+
+    public static OrderController getOrderController() {
+        return new OrderController(getOrderService(), getProductService(), getInputView(), getOutputView());
+    }
+
+    private static OrderService getOrderService() {
+        return new OrderService(getOderRepository(), getProductRepository());
+    }
+
+    private static OrderRepository getOderRepository() {
+        return new OrderRepository();
+    }
+
+    private static InputView getInputView() {
+        return new InputView();
+    }
+
+    private static OutputView getOutputView() {
+        return new OutputView();
+    }
+
+}
