61b174817df894aa6b9ca64cbd3fef2678de427b
JuSeong1130 <jsmoon1130@naver.com>
2024-11-11 20:14:45 +0900

    feat(Order) : 영수증 구하는 기능 구현

--- src/main/java/store/domain/Order.java
+++ src/main/java/store/domain/Order.java
@@ -25,4 +25,54 @@ public class Order {
         this.hasMembershipDiscount = true;
         this.orderState = OrderState.SUCCESS;
     }
+
+    public OrderReceipt getReceipt() {
+        List<OrderGift> orderGifts = getOrderGifts();
+        OrderPayment orderPayment = getOrderPayment();
+        return new OrderReceipt(orderProducts, orderGifts, orderPayment);
+    }
+
+    private OrderPayment getOrderPayment() {
+        int totalAmount = calculateTotalAmount();
+        int promotionDiscount = calculatePromotionDiscount();
+        int memberShipDiscount = calculateMembershipDiscount(totalAmount, promotionDiscount);
+        int payment = totalAmount - promotionDiscount - memberShipDiscount;
+        int totalCount = calculateTotalCount();
+
+        return new OrderPayment(totalCount, totalAmount, promotionDiscount, memberShipDiscount, payment);
+    }
+
+    private int calculateTotalAmount() {
+        return orderProducts.stream()
+                .mapToInt(OrderProduct::getTotal)
+                .sum();
+    }
+
+    private int calculatePromotionDiscount() {
+        return orderProducts.stream()
+                .mapToInt(OrderProduct::getPromotionDiscount)
+                .sum();
+    }
+
+    private int calculateMembershipDiscount(int totalAmount, int promotionDiscount) {
+        if (!hasMembershipDiscount) {
+            return 0;
+        }
+        int calculatedDiscount = (int) ((totalAmount - promotionDiscount) * 0.3);
+        return Math.min(calculatedDiscount, 8000);
+    }
+
+    private int calculateTotalCount() {
+        return orderProducts.stream()
+                .mapToInt(OrderProduct::getCount)
+                .sum();
+    }
+
+    private List<OrderGift> getOrderGifts() {
+        //product 돌면서 promotionQuantity랑 이름 추출
+        return orderProducts.stream()
+                .filter(OrderProduct::hasGift)
+                .map(orderProduct -> new OrderGift(orderProduct.getName(), orderProduct.getGiftCount()))
+                .toList();
+    }
 }
