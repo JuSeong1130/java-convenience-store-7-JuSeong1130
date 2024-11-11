07796bd77f6eb699689a330bf3865f788c2123cd
JuSeong1130 <jsmoon1130@naver.com>
2024-11-11 20:36:47 +0900

    fix(ResponseProducts) : 출력 버그 수정
    프로모션 상품이 있는 것은 일반상품 재고가 없어도 출력되어야했는데 출력되지 않아 수정

--- src/main/java/store/view/dto/ResponseProducts.java
+++ src/main/java/store/view/dto/ResponseProducts.java
@@ -10,8 +10,15 @@ public record ResponseProducts(List<ResponseProduct> products) {
     public static ResponseProducts from(List<Product> products) {
         List<ResponseProduct> responseProducts = new ArrayList<>();
         for (Product product : products) {
-            responseProducts.add(new ResponseProduct(product.getName(), product.getPrice(), product.getPromotionQuantity(), product.getPromotionName()));
-            responseProducts.add(new ResponseProduct(product.getName(), product.getPrice(), product.getCommonQuantity(), ""));
+            if(product.getPromotionQuantity() != 0) {
+                responseProducts.add(new ResponseProduct(product.getName(), product.getPrice(), product.getPromotionQuantity(), product.getPromotionName()));
+                responseProducts.add(new ResponseProduct(product.getName(), product.getPrice(), product.getCommonQuantity(), ""));
+                continue;
+            }
+
+            if(product.getCommonQuantity() != 0) {
+                responseProducts.add(new ResponseProduct(product.getName(), product.getPrice(), product.getCommonQuantity(), ""));
+            }
         }
         return new ResponseProducts(responseProducts);
     }
